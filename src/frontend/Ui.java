c5efd20d7b51f639e067b4ffcd7299d047d4a2e7
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-01-22 10:31:56 +0100

    Ui Init with Main setup

---
+++ src/frontend/Ui.java
@@ -0,0 +1,14 @@
+//Packages
+package src.frontend;
+
+//Imports
+import javax.swing.JFrame;
+
+//Ui Code
+public class Ui extends JFrame{
+    public Ui(){
+        //Frame Settings
+        setSize(1280, 960);
+        setTitle("Swift Share");
+    }
+}
