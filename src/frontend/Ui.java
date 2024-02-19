887100ca0b34671fb650b57b6d233ff814e66912
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-02-19 12:45:27 +0100

    Small layout edits and TODOs add

--- src/frontend/Ui.java
+++ src/frontend/Ui.java
@@ -2,13 +2,40 @@
 package src.frontend;
 
 //Imports
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 //Ui Code
 public class Ui extends JFrame{
+    //Components erstellung
+    JPanel Main_Background_Panel = new JPanel();
+    JPanel Main_Title_Panel = new JPanel();
+    JLabel Main_Title_Label = new JLabel();
     public Ui(){
+
         //Frame Settings
         setSize(1280, 960);
         setTitle("Swift Share");
+
+        //Logo Icon TODO:Ein logo icon für das Window muss noch eingebaut werden
+
+        //Background
+        add(Main_Background_Panel);
+        Main_Background_Panel.setLayout(new GridBagLayout());
+        Main_Background_Panel.setBackground(new Color(51, 51, 153));
+        Main_Background_Panel.setOpaque(true);
+
+        //Title Panel TODO: Title Panel muss erstellt werden
+        Main_Background_Panel.add(Main_Title_Panel);
+        Main_Background_Panel.setLayout(new GridLayout(1,2));
+        //Logo  TODO: Logo muss noch eingefügt werden
+
+        //Title TODO: Title muss eingefügt werden mit richtige font
+        Main_Title_Panel.add(Main_Title_Label);
+        Main_Title_Label.setText("SwiftShare");
+
+
+
+        setVisible(true);
     }
 }
---
+++ src/testumgebung/Sender.java
@@ -0,0 +1,4 @@
+package src.testumgebung;
+
+public class Sender {
+}
