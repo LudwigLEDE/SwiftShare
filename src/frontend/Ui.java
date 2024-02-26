20f0fe4b2707bcd02b23a9fa87e6deac01fccc55
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-02-26 12:46:15 +0100

    Einbau der ganzen Panels

--- src/frontend/Ui.java
+++ src/frontend/Ui.java
@@ -2,8 +2,13 @@
 package src.frontend;
 
 //Imports
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import java.awt.GridBagLayout;
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 
@@ -21,6 +26,7 @@ public class Ui extends JFrame{
     JPanel Main_Friends_Panel = new JPanel();
     JLabel Main_Friends_Label = new JLabel();
     JButton Main_Friends_Add_Button = new JButton();
+    JTable Main_Friends_Table = new JTable();
 
     //Files Panel
     JPanel Main_Files_Panel = new JPanel();
@@ -40,6 +46,7 @@ public class Ui extends JFrame{
         setSize(1280, 960);
         setTitle("Swift Share");
 
+        /*
         //Logo Icon Erstellen TODO:Ein logo icon für das Window muss noch eingebaut werden
         ImageIcon Logo = new ImageIcon("src/assets/img/Logo.png");
         Main_Title_Logo_Label.setIcon(Logo);
@@ -53,6 +60,7 @@ public class Ui extends JFrame{
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        */
 
         //Background
         add(Main_Background_Panel);
@@ -60,10 +68,11 @@ public class Ui extends JFrame{
         Main_Background_Panel.setBackground(new Color(51, 51, 153));
         Main_Background_Panel.setOpaque(true);
 
+        /*      TODO: Muss weider eingeführt werden
+
         //Title Panel TODO: Title Panel muss erstellt werden
         Main_Background_Panel.add(Main_Title_Panel);
         Main_Background_Panel.setLayout(new GridLayout(1,2));
-        //Logo  TODO: Logo muss noch eingefügt werden
 
         //Logo zum Panel zuweisung
         Main_Title_Panel.add(Main_Title_Logo_Label);
@@ -72,6 +81,22 @@ public class Ui extends JFrame{
         Main_Title_Panel.add(Main_Title_Label);
         Main_Title_Label.setForeground(Color.BLACK); //TODO: Muss weiß werden wenn background aktiv ist
         Main_Title_Label.setText("SwiftShare");
+*/
+
+        //File Select Panel
+        Main_Files_Panel.setLayout(new GridBagLayout());
+        Main_Files_Panel.add(Main_Files_Label);
+        Main_Files_Panel.add(Main_Files_Add_Button);
+        Main_Files_Panel.add(Main_Files_Clear_Button);
+        Main_Files_Panel.add(Main_Files_SelectedFiles_Table);
+        add(Main_Files_Panel);
+
+        //Friend List
+        Main_Friends_Panel.setLayout(new GridBagLayout());
+        Main_Friends_Panel.add(Main_Friends_Label);
+        Main_Friends_Panel.add(Main_Friends_Add_Button);
+        Main_Friends_Panel.add(Main_Friends_Table);
+        add(Main_Friends_Panel);
 
         setVisible(true);
     }
