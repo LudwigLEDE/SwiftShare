35736cbb49a9dfc95a0a16a4af1d283c63bd669c
l.engelhardt <l.engelhardt@edu.letteverein.berlin>
2024-04-15 11:00:31 +0200

    new server ports

--- src/backend/SenderServer.java
+++ src/backend/SenderServer.java
@@ -6,7 +6,9 @@ import java.net.Socket;
 //Ports die wir nutzen: 50000, 50001, 50002
 public class SenderServer { // FileOwnerServer
 
-    private static  final int SERVER_PORT = 12345;
+    private static final int SERVER_PORT = 50000;
+    private static final int SERVER_PORT_2 = 50001;
+    private static final int SERVER_PORT_3 = 50002;
     public static void main(String[] args) {
         try {
             // Obtain the local host's IP address
